d3a25e51da87e4e89f7edfc1fdebafe8f3bd8caf
BriianG <brian.garcia7900@gmail.com>
2022-05-31 14:55:36 +0200

    se agrego .env

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Cuerpo from './components/cuerpo/cuerpo'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './components/NavBar/NavBar';
+
+
+function App() {
+return (
+   <>
+   <NavBar/>
+   <Cuerpo/>
+   </>
+)
+}
+
+export default App;
